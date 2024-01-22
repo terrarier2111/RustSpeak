0d72ded2c93cd2736bd5b2fc51e22d99a196d845
threadexception <hannes.gaumann@outlook.de>
2024-01-22 22:55:26 +0100

    Update dependencies

--- server/src/certificate.rs
+++ server/src/certificate.rs
@@ -11,19 +11,24 @@ pub(crate) mod secure_authority {
         priv_key_file: File,
     ) -> anyhow::Result<(Vec<Certificate>, PrivateKey)> {
         let mut cert_chain_reader = BufReader::new(cert_file);
-        let certs = rustls_pemfile::certs(&mut cert_chain_reader)?
-            .into_iter()
-            .map(Certificate)
-            .collect();
+        let certs_iter = rustls_pemfile::certs(&mut cert_chain_reader);
+        let mut certs = Vec::with_capacity(certs_iter.size_hint().0);
+        for cert in certs_iter {
+            certs.push(Certificate(cert?.as_ref().to_vec()));
+        }
 
         let mut key_reader = BufReader::new(priv_key_file);
         // if the file starts with "BEGIN RSA PRIVATE KEY"
         // let mut key_vec = rustls_pemfile::rsa_private_keys(&mut reader)?;
         // if the file starts with "BEGIN PRIVATE KEY"
-        let mut keys = rustls_pemfile::pkcs8_private_keys(&mut key_reader)?;
+        let key_iter = rustls_pemfile::pkcs8_private_keys(&mut key_reader);
+        let mut keys = Vec::with_capacity(key_iter.size_hint().0);
+        for key in key_iter {
+            keys.push(key?);
+        }
 
         assert_eq!(keys.len(), 1);
-        let key = PrivateKey(keys.remove(0));
+        let key = PrivateKey(keys.get(0).unwrap().secret_pkcs8_der().to_vec());
 
         Ok((certs, key))
     }
